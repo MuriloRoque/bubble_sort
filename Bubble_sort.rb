d0e1ebb1ac3dba99a7f8657a6b538a47a1feb4da
Murilo Roque <muriloengqui@gmail.com>
2020-04-06 11:47:33 -0300

    Fixed stickler errors

--- Bubble_sort.rb
+++ Bubble_sort.rb
@@ -1,24 +1,20 @@
 def bubble_sort(arr)
-  arr.each do | i|
+  arr.each do |i|
     j = 0
     while j < arr.length - 1
-      if arr[j] > arr[j + 1]
-        arr[j], arr[j + 1] = arr[j + 1], arr[j]
-      end
-      j+=1
+      arr[j], arr[j + 1] = arr[j + 1], arr[j] if arr[j] > arr[j + 1]
+      j += 1
     end
   end
   arr
 end
 
 def bubble_sort_by(arr)
-  arr.each do | i|
+  arr.each do |i|
     j = 0
     while j < arr.length - 1
-      if yield(arr[j], arr[j + 1]).positive?
-        arr[j], arr[j + 1] = arr[j + 1], arr[j]
-      end
-      j+=1
+      arr[j], arr[j + 1] = arr[j + 1], arr[j] if yield(arr[j], arr[j + 1]).positive?
+      j += 1
     end
   end
   arr
@@ -26,6 +22,6 @@ end
 
 puts bubble_sort([4, 3, 78, 2, 0, 2])
 
-bubble_sort_by(["hi", "hello", "hey"]) do |left, right|
+bubble_sort_by(%w[hi hello hey]) do |left, right|
   left.length - right.length
 end
