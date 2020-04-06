698d9446c45c85422784a77e759f42f3a624d5bf
Murilo Roque <muriloengqui@gmail.com>
2020-04-06 06:49:56 -0300

    Added string bubble sort

--- bubble-sort.rb
+++ bubble-sort.rb
@@ -9,4 +9,19 @@ def bubble_sort(arr)
 return arr
 end
 
-  puts bubble_sort([4,3,78,2,0,2])
+def bubble_sort_by(arr)
+  for i in 0...arr.length
+    for j in 0...arr.length-i-1
+      if yield(arr[j],arr[j+1]) > 0
+        arr[j], arr[j+1] = arr[j+1], arr[j]
+      end
+    end
+  end
+return arr
+end
+
+bubble_sort([4,3,78,2,0,2])
+
+bubble_sort_by(["hi","hello","hey"]) do |left,right|
+  left.length - right.length
+end
